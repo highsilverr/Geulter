d6bb82ab7fc1d30e0a3d419d8be5fc28b92c8be1
highsiverr <ye90202@gmail.com>
2025-08-07 23:20:39 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/geulter/story/GeulterApplication.java
@@ -0,0 +1,13 @@
+package com.geulter.story;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeulterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeulterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geulter/story/GeulterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geulter.story;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeulterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
