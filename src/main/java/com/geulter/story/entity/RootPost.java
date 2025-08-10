3290e13120e2ede91129aeb0d11ba64cacc6a835
highsiverr <ye90202@gmail.com>
2025-08-10 22:56:40 +0900

    feat: RootPost of 추가

--- src/main/java/com/geulter/story/entity/RootPost.java
+++ src/main/java/com/geulter/story/entity/RootPost.java
@@ -1,10 +1,17 @@
 package com.geulter.story.entity;
 
+import com.geulter.story.entity.dto.RootPostRequestDTO;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.ZonedDateTime;
 
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RootPost {
 
     @Id
@@ -24,4 +31,13 @@ public class RootPost {
     @JoinColumn(name = "user_id", nullable = true)
     private User user;
 
+    public static RootPost of(String title, String content, String rootPostWriter, User user){
+        return builder()
+                .title(title)
+                .content(content)
+                .createdAt(ZonedDateTime.now())
+                .rootPostWriter(rootPostWriter)
+                .user(user)
+                .build();
+    }
 }
